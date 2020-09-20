0b9d5540056d19ff6c243e947e3647a5837cf331
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-09-20 18:49:11 -0400

    Update cancel_request.js

--- Cancel_Request/cancel_request.js
+++ Cancel_Request/cancel_request.js
@@ -1,5 +1,5 @@
 var inputs = document.getElementsByClassName('_54k8 _52jg _56bs _26vk _2b4n _8yzq _3cqr _8yo0 _56bt');
-for (var i = 0; i < 10; i++) {
+for (var i = inputs.length - 1; i >= inputs.length - 10; i--) {
     inputs[i].click();
     setTimeout(function() {
         location.reload();
