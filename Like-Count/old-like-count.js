8becc1362861afd5ed24a1d7b24d79b8e25893bc
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-09-27 13:21:47 -0400

    Update old-like-count.js

--- Like-Count/old-like-count.js
+++ Like-Count/old-like-count.js
@@ -2,12 +2,13 @@ var a = document.documentElement.outerHTML;
 var b = a.match(/<div class="_5i_p" id="u_.*?_.*?">.*?"><\/span><\/div><\/div>/g)
 document.write(b[0]);
 var c = document.documentElement.outerHTML;
+var v = c.match(/(?<=src=").*?(?=")/g);
 var e = c.match(/(?<=<a title=").*?(?=")/g)
 var d = c.match(/(?<=data-gt="{.*?}}" href=")http.*?(?=\?fref|&amp)/g);
 document.open();
 document.clear();
 document.close();
 var f = prompt("请输入点赞类型", "");
-for (var k = 0, j = 0; k < d.length, j < e.length; k++, j++) {
-    document.write("<table><tbody><tr><td>" + f + "</td><td>" + e[j] + "</td><td>" + d[k] + "</td></tr></tbody></table>");
+for (var k = 0, j = 0, h =0; k < d.length, j < e.length, h < v.length; k++, j++, h++) {
+    document.write("<table><tbody><tr><td>=IMAGE(\"" +v[h]+ "\")</td><td>" + f + "</td><td>" + e[j] + "</td><td>" + d[k] + "</td></tr></tbody></table>");
 }
