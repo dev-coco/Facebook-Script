1e5f1bc5f073d32cab9c90a33bf843de37bb4ec5
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-10-08 10:29:17 -0400

    Update auto-scroll.js

--- Auto-Scroll/auto-scroll.js
+++ Auto-Scroll/auto-scroll.js
@@ -2,15 +2,15 @@ function getRandom(n, m) {
     var num = Math.floor(Math.random() * (m - n + 1) + n)
     return num
 }
-var ramdom_number = getRandom(5, 30)
+var ramdom_number = getRandom(5, 15);
 if (enbledscroll == '1') {
-    var enbledscroll = '0';
+    var enbledscroll = 0;
     clearInterval(autoscroll);
 } else {
     var enbledscroll = 1;
     var position = document.body.scrollHeight;
     var autoscroll = setInterval(function() {
-        position += 10;
+        position += ramdom_number;
         scroll(0, position);
-    }, getRandom);
+    }, ramdom_number);
 }
