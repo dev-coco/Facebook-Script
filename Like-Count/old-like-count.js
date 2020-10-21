ae64fb6faac8441a84d001db7a47ee916bade21c
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-10-20 22:11:00 -0400

    Update old-like-count.js

--- Like-Count/old-like-count.js
+++ Like-Count/old-like-count.js
@@ -2,13 +2,14 @@ var a = document.documentElement.outerHTML;
 var b = a.match(/<div class="_5i_p" id="u_.*?_.*?">.*?"><\/span><\/div><\/div>/g)
 document.write(b[0]);
 var c = document.documentElement.outerHTML;
-var v = c.match(/(?<=src=").*?(?=")/g);
-var e = c.match(/(?<=<a title=").*?(?=")/g)
-var d = c.match(/(?<=data-gt="{.*?}}" href=")http.*?(?=\?fref|&amp)/g);
+var v = c.match(/(?<=<a class="_5i_s _8o _8r lfloat _ohe" title=").*?\?id=.*?(?=&amp;extragetparams)/g);
 document.open();
 document.clear();
 document.close();
 var f = prompt("请输入点赞类型", "");
-for (var k = 0, j = 0, h =0; k < d.length, j < e.length, h < v.length; k++, j++, h++) {
-    document.write("<table><tbody><tr><td>=IMAGE(\"" +v[h]+ "\")</td><td>" + f + "</td><td>" + e[j] + "</td><td>" + d[k] + "</td></tr></tbody></table>");
+for (var k = 0; k < v.length; k++) {
+    var userid = v[k].match(/(?<=user\.php\?id=)[0-9].+/g);
+    var username = v[k].match(/.*?(?=")/g)[0];
+    var userurl = v[k].match(/http.+(?=(\?|&amp;)fref)/g);
+    document.write("<table><tbody><tr><td>=IMAGE(\"http://graph.facebook.com/" + userid + "/picture?width=1080\")</td><td>" + f + "</td><td><a href=\"" + userurl + "\">" + username + "</a></td><td>" + userurl + "</td></tr></tbody></table>");
 }
