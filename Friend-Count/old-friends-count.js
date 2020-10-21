4b5de10f6d764b44b660f2f4be3e38e50432047b
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-10-20 22:11:44 -0400

    Update old-friends-count.js

--- Friend-Count/old-friends-count.js
+++ Friend-Count/old-friends-count.js
@@ -1,20 +1,19 @@
-var a = document.documentElement.outerHTML;
-var b = a.match(/<div class="_5i_p" id="u_.*?_.*?">.*?"><\/span><\/div><\/div>/g)
-document.write(b[0]);
-var c = document.documentElement.outerHTML;
-var v = c.match(/(?<=data-hovercard="\/ajax\/hovercard\/user\.php\?id=).*?(?=&)/g);
-var e = c.match(/(?<=<a title=").*?(?=")/g)
-var d = c.match(/(?<=data-gt="{.*?}}" href=")http.*?(?=\?fref|&amp)/g);
-document.open();
-document.clear();
-document.close();
-var f = prompt("请输入点赞类型", "");
-for (var k = 0, j = 0, h =0; k < d.length, j < e.length, h < v.length; k++, j++, h++) {
-    document.write("<table><tbody><tr><td>=IMAGE(\"http://graph.facebook.com/" +v[h*2]+ "/picture?width=1080\")</td><td>" + f + "</td><td><a href=\"" + d[k] + "\">" + e[j] + "</a></td><td>" + d[k] + "</td></tr></tbody></table>");
+var html = document.documentElement.outerHTML;
+var c = html.match(/(?<=<div class="fsl fwb fcb"><a href=").*?(?=<\/a><\/div>)/g);
+var account = html.match(/(?<=<span class="_1vp5">).*?(?=<\/span>)/g);
+var g = prompt("请输入名字", "");
+var date = new Date();
+var nowDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+for (var i = 0; i < c.length; i++) {
+    var url = c[i].match(/https:\/\/www\.facebook.*?(?=.fref|&amp)/g);
+    var k = c[i].match(/(?<=data-hovercard-prefer-more-content.*?>).+/g);
+    document.write("<table><tbody><tr><td>" + nowDate + "</td><td>" + g + "</td><td>" + account + "</td><td></td><td>" + [i] + "</td><td>" + k + "</td><td></td><td></td><td>" + url + "</td></tr></tbody></table>");
 }
-var g = document.documentElement.outerHTML;
+var d = document.documentElement.outerHTML;
+var e = d.replace(/<div class="simplemarker-mark" style="background-color: rgba\(255, 255, 255, 0\); display: inline; text-align: center; color: red; padding: 2px;">|<\/div>/g, "");
 document.open();
 document.clear();
 document.close();
-var u = g.replace(/undefined.+/g,"").replace(/[^>]+(?!.*<table)>.+/g,"");
-document.write(u);
+for (var i = 0; i < e.length; i++) {
+    document.write(e[i]);
+}
