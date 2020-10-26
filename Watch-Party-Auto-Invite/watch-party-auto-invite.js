a54f8ce412975c03791e8ab28e3484ee5f7f8c6f
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-10-26 08:39:35 -0400

    Update watch-party-auto-invite.js

--- Watch-Party-Auto-Invite/watch-party-auto-invite.js
+++ Watch-Party-Auto-Invite/watch-party-auto-invite.js
@@ -1,11 +1,10 @@
-var inputs = document.getElementsByClassName('_71u9 _4jy0 _4jy3 _517h _51sy _42ft');
+var inputs = document.querySelectorAll('[class*="_71u9"][class*="_4jy0"][class*="_4jy3"][class*="_517h"][class*="_51sy"][class*="_42ft"]');
     function getRandom (n, m) {
     var num = Math.floor(Math.random() * (m - n + 1) + n)
     return num
 }
-var ramdom_number = getRandom(18000, 25000)
+var ramdom_number = getRandom(10000, 20000)
 for (var i=0;i<inputs.length;i++){
-    
         (function(i) {
         setTimeout(function() {
         inputs[i].click();
