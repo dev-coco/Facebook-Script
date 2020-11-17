e84a765538530942453fc51681052202c8237543
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-11-16 20:42:47 -0400

    Update new-likes-count.js

--- Like-Count/new-likes-count.js
+++ Like-Count/new-likes-count.js
@@ -1,5 +1,5 @@
 var a = document.documentElement.outerHTML;
-var b = a.match(/<div class="j83agx80 cbu4d94t buofh1pr">.*?k4urcfbm"><\/div><\/div><\/div>/g)
+var b = a.match(/<div class="j83agx80 cbu4d94t buofh1pr l9j0dhe7">.*?k4urcfbm"><\/div><\/div><\/div>/g)
 for (var i = 0; i < b.length; i++) {
     document.write(b[i]);
 }
@@ -12,7 +12,7 @@ document.clear();
 document.close();
 var f = prompt("请输入点赞类型", "");
 for (var k = 0, j = 0, h = 0; k < d.length, j < e.length, h < v.length; k++, j++, h++) {
-    document.write("<table><tbody><tr><td>=IMAGE(\"" +v[h]+ "\")</td><td>" + f + "</td><td>" + e[j] + "</td><td>" + d[k] + "</td></tr></tbody></table>");
+    document.write("<table><tbody><tr><td>=IMAGE(\"" +v[h]+ "\")</td><td>" + f + "</td><td><a href=\""+ d[k] +"\">" + e[j] + "</a></td><td>" + d[k] + "</td></tr></tbody></table>");
 }
 var g = document.documentElement.outerHTML;
 document.open();
