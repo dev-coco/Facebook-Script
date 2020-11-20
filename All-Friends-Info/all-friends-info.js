40da6a5edba625836200d79c8f7c7942640fffd2
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-11-20 10:49:16 -0400

    Update all-friends-info.js

--- All-Friends-Info/all-friends-info.js
+++ All-Friends-Info/all-friends-info.js
@@ -5,5 +5,5 @@ var userid = getdata.match(/(?<=id":").*?(?=")/g);
 document.write('<table><tbody>');
 for (var i = 0; i < userid.length; i++) {
 var result = eval('`'  + username[i] + '`');
-    document.write('<tr><td><img src="https://graph.facebook.com/' + userid[i] + '/picture?width=100&access_token=' + token + '" style="width:100;height:100px;"></td><td><a href="https://www.facebook.com/' + userid[i] + '">' + result + '</td><td>' + userid[i] + '</td></tr>');
+    document.write('<tr><td><img src="https://graph.facebook.com/' + userid[i] + '/picture?width=100&access_token=' + token + '" style="width:100;height:100px;"></td><td>'+[i+1]+'</td><td><a href="https://www.facebook.com/' + userid[i] + '">' + result + '</td><td>' + userid[i] + '</td></tr>');
 }
