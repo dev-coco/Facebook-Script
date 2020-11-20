98615ae84dbbbe45141c57b9a0138fca2d2d8aed
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-11-20 17:27:46 -0400

    Update new-group-info.js

--- Group-Info/new-group-info.js
+++ Group-Info/new-group-info.js
@@ -5,7 +5,7 @@ var last_month_posts = a.match(/(?<=number_of_posts_in_last_month":).*?(?=,")/g)
 var all_member = a.match(/(?<=group_member_profiles.*?count":).*?(?=\})/g)[0];
 var new_member = a.match(/(?<=group_new_member_profiles":{"count":).*?(?=\})/g)[0];
 var group_status = a.match(/(?<=privacy_info.*?text":").*?(?="\})/g)[0];
-var group_name = a.match(/(?<=group_new_member_profiles.*?"name":").*?(?=","email_domains)/g)[0];
+var group_name = a.match(/(?<=isProfile":"Group","name":").*?(?=",")/g)[0];
 var url = (location.href+"/").match(/http.+oups\/.*?\//g);
 var keyword = RegExp(/god|bible|church|fellowship|christ|baptist|presbyterian|gospel|catholic|holy|jesus|lord|ministry|kingdom|religion/);
 var check_keyword = keyword.test(group_name.toLowerCase());
@@ -18,4 +18,3 @@ var date = new Date();
 var today = (date.getMonth() + 1) + "/" + date.getDate();
 var str ="<table><tbody><tr><td>"+today+"</td><td></td><td></td><td>"+group_location+"</td><td>"+religion+"</td><td>"+group_name+"</td><td></td><td>"+url+"</td><td>"+all_member+"</td><td>"+last_month_posts+"</td><td>"+new_member+"</td><td>"+group_status+"</td></tr></tbody></table>"
 document.write(eval("'" + str + "'"));
-
