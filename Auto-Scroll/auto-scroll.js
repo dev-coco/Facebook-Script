7e36c83f6bf60997e3c821b56ce1548a4943e23a
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-12-03 00:07:31 -0400

    Update auto-scroll.js

--- Auto-Scroll/auto-scroll.js
+++ Auto-Scroll/auto-scroll.js
@@ -1,8 +1,8 @@
 if (enbledscroll == '1') {
-    var enbledscroll = '0';
+    var enbledscroll = 0;
     clearInterval(autoscroll);
 } else {
-    var enbledscroll = '1';
+    var enbledscroll = 1;
     var autoscroll = setInterval(function() {
         window.scrollTo(0, document.body.scrollHeight)
     }, 1500);
