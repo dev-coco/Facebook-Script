aef699df428ef2606c0d2265acbbdb5217db5626
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-12-25 19:40:36 -0400

    Update find-fb-number-id.js

--- Find-FB-ID/find-fb-number-id.js
+++ Find-FB-ID/find-fb-number-id.js
@@ -5,14 +5,8 @@ for (var k = 0; k < url.length; k++) {
         setTimeout(async function() {
             let response = await fetch(url[k])
             let text = await response.text()
-            let matches = text.match(/"userVanity":".*?"/g)
-            let facebookID = matches ? matches.sort((a, b) => matches.filter(v => v == a).length - matches.filter(v => v == b).length).pop().slice('"userVanity":"'.length).slice(0, -1) : ''
-            if (facebookID.length == 0) {
-                facebookID = url[k].replace(/.+com\//g, "");
-                document.write("<a href=\"https://www.facebook.com/profile.php?id=" + facebookID + "\">https://www.facebook.com/profile.php?id=" + facebookID + "</a><br>");
-            } else {
-                document.write("<a href=\"https://www.facebook.com/" + facebookID + "\">https://www.facebook.com/" + facebookID + "</a><br>");
-            }
+            let facebookID = text.match(/"userID":"([0-9]+)/g)[0]
+            document.write(""+facebookID+"<br>");
         }, 1500 * k);
     })(k);
 }
