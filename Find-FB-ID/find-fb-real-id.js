ef710fee4337f747060769c0f7cfb4a69ecf947b
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-12-28 21:32:21 -0400

    Update find-fb-real-id.js

--- Find-FB-ID/find-fb-real-id.js
+++ Find-FB-ID/find-fb-real-id.js
@@ -5,15 +5,17 @@ for (var k = 0; k < url.length; k++) {
         setTimeout(async function() {
             let response = await fetch(url[k])
             let text = await response.text()
-try {
-            let facebookID = text.match(/(?<="userVanity":").*?(?=")/g)[0]
-            if (facebookID.length == 0) {
-                facebookID = url[k].replace(/.+com\//g, "");
-                document.write("<a href=\"https://www.facebook.com/profile.php?id=" + facebookID + "\">https://www.facebook.com/profile.php?id=" + facebookID + "</a><br>");
-            } else {
-                document.write("<a href=\"https://www.facebook.com/" + facebookID + "\">https://www.facebook.com/" + facebookID + "</a><br>");
+            try {
+                let facebookID = text.match(/(?<="userVanity":").*?(?=")/g)[0]
+                if (facebookID.length == 0) {
+                    facebookID = url[k].replace(/.+com\//g, "");
+                    document.write("<a href=\"https://www.facebook.com/profile.php?id=" + facebookID + "\">https://www.facebook.com/profile.php?id=" + facebookID + "</a><br>");
+                } else {
+                    document.write("<a href=\"https://www.facebook.com/" + facebookID + "\">https://www.facebook.com/" + facebookID + "</a><br>");
+                }
+            } catch {
+                document.write("<br>");
             }
-} catch {document.write("<br>");}
         }, 1500 * k);
     })(k);
 }
