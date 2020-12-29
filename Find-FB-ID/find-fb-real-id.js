956bb1b810d03ceccb7d3a9a14c6eba5d5db94ab
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-12-28 21:31:54 -0400

    Update find-fb-real-id.js

--- Find-FB-ID/find-fb-real-id.js
+++ Find-FB-ID/find-fb-real-id.js
@@ -1,10 +1,11 @@
 var person = prompt("请输入", "");
-var url = person.match(/https:\/\/[A-Za-z0-9&/?=.]+[A-Za-z0-9/]/g);
+var url = person.match(/[0-9].+/g);
 for (var k = 0; k < url.length; k++) {
     (function(k) {
         setTimeout(async function() {
             let response = await fetch(url[k])
             let text = await response.text()
+try {
             let facebookID = text.match(/(?<="userVanity":").*?(?=")/g)[0]
             if (facebookID.length == 0) {
                 facebookID = url[k].replace(/.+com\//g, "");
@@ -12,6 +13,7 @@ for (var k = 0; k < url.length; k++) {
             } else {
                 document.write("<a href=\"https://www.facebook.com/" + facebookID + "\">https://www.facebook.com/" + facebookID + "</a><br>");
             }
+} catch {document.write("<br>");}
         }, 1500 * k);
     })(k);
 }
