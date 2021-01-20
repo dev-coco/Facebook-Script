21f58caf091547bd974f49e41be9fae5d959f52e
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-01-20 10:30:52 -0400

    Update find-fb-real-id.js

--- Find-FB-ID/find-fb-real-id.js
+++ Find-FB-ID/find-fb-real-id.js
@@ -6,7 +6,7 @@ for (var k = 0; k < get_url.length; k++) {
             let response = await fetch("https://www.facebook.com/"+get_url[k])
             let real_url = response.url.replace(/\/$/g,"");
             try {
-              document.write(`<table><tbody><tr><td>`+[k+1]+`</td><td><a href="` + real_url + `">` + real_url + `</a></td></tr></tbody></table>`);
+                document.write(`<table><tbody><tr><td>`+[k+1]+`</td><td><a href="` + real_url + `">` + real_url + `</a></td></tr></tbody></table>`);
             } catch {
                 document.write(`<table><tbody><tr><td>`+[k+1]+`</td><td>识别不到</td></tr></tbody></table>`);
             }
