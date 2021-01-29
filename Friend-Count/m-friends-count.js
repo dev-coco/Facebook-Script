e312b600b28f7b88031071e91f6aee388efbdd21
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-01-29 10:16:50 -0400

    Update m-friends-count.js

--- Friend-Count/m-friends-count.js
+++ Friend-Count/m-friends-count.js
@@ -1,8 +1,8 @@
 var get_data = document.getElementsByClassName("_56cx acw");
 new_page = window.open('');
-for (var i=0;i<get_data.length;i++)
-{
+for (var i=get_data.length - 1;i<get_data.length;i--)
+{ 
     var date = get_data[i].outerHTML.match(/(?<=<section class="_56cz _56c_">).*?(?=<\/section>)/g)[0];
     var friend_count = get_data[i].getElementsByClassName("img darkTouch profpic").length;
-    new_page.document.write(`<table><tbody><tr><td>`+date+`</td><td>`+friend_count+`</td></tr></tbody></table>`);
+    new_page.document.write(`<table><tbody><tr><td style="user-select:none;">`+date+`</td><td>`+friend_count+`</td></tr></tbody></table>`);
 }
