26a2e46627ca36eee3bc6aa540758116bef7a361
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-06-11 23:32:32 -0400

    Update friends-group.js

--- Friends-Group/friends-group.js
+++ Friends-Group/friends-group.js
@@ -5,7 +5,7 @@ for (var i = 0; i < get_data.length; i++) {
     var group_name = get_data[i].outerHTML.match(/(?<=aria-label=").*?(?=")/g)[0];
     var group_url = get_data[i].outerHTML.match(/https:\/\/www\.facebook\.com\/groups.*?(?=")/g)[0];
     var group_info = get_data[i].outerHTML.match(/(?<=g0qnabr5">).*?(?=<\/span>)/g)[1];
-    var group_members = group_info.replace(/(?<=\.[0-9])&nbsp;万.+/g, "000").replace(/&nbsp;|\./g, "").replace(/万.+/g, "0000");
-    var post_count = group_info.replace(/.+：| 篇/g, "");
+    var group_members = group_info.replace(/(?<=\.[0-9])&nbsp;(万|萬).+/g, "000").replace(/(万|萬).+/g, "0000").replace(/&nbsp;|\.|位.+/g, "")
+    var post_count = group_info.replace(/.+：|.+一.* | 篇|則.+/g, "");
     new_page.document.write("<table><tbody><tr><td>" + group_name + "</td><td>" + group_url + "</td><td>" + group_members + "</td><td>" + post_count + "</td></tr></tbody></table>");
 }
