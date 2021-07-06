0cfd8e0eb8c4242462fc0335c039d3a316e718fc
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-07-06 12:52:14 -0400

    Delete Reorganize-Hyperlink directory

--- Reorganize-Hyperlink/reorganize-hyperlink.js
+++
@@ -1,7 +0,0 @@
-var person = prompt("请输入", "");
-var url = person.match(/https:\/\/[A-Za-z0-9&/?=.]+[A-Za-z0-9/]/g);
-var name = person.replace(/https:\/\/[A-Za-z0-9&/?=.]+[A-Za-z0-9/]/g, "\n");
-var nameb = name.match(/.+/g);
-for (var k = 0, j = 0; k < url.length, j < nameb.length; k++, j++) {
-    document.write("<a href=\"" + url[k] + "\">" + nameb[j] + "</a><br>");
-}
