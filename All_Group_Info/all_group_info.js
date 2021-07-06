1b4982be7735d837ea8bcb51c1d06205561ee219
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-07-06 12:49:06 -0400

    Delete All_Group_Info directory

--- All_Group_Info/all_group_info.js
+++
@@ -1,7 +0,0 @@
-var html = document.documentElement.outerHTML;
-var get_data = html.match(/(?<=<div class="_7hkf _3qn7 _61-3 _2fyi _3qng">).*?(?=<\/div>)/g);
-for (var i=0;i<get_data.length;i++){
-var group_name = get_data[i].match(/(?<=<div class=" _52je _52jb _52jh">).*/g);
-var group_url = get_data[i].match(/(?<=<a class="_7hkg" href=").*?(?=\/\?ref=group_browse">)/g);
-    document.write("<table><tbody><tr><td>"+group_name+"</td><td>https://www.facebook.com"+group_url+"</td></tr></tbody></table>");
-}
