555bd583a9682662b69dac84acd439967c573a9d
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-07-06 12:52:08 -0400

    Delete Group-Smart-Search directory

--- Group-Smart-Search/group-smart-search.js
+++
@@ -1,4 +0,0 @@
-var a = location.href;
-var b = prompt("请输入关键词", "");
-var c = a.match(/(?<=filters=).+/g);
-window.location.href = "https://www.facebook.com/search/groups?q=" + b + "&filters=" + c;
