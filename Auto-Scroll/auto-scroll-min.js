e7dd6f69375778c2cb178e6ac4a5980bb6c176c9
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-07-06 12:50:12 -0400

    Delete Auto-Scroll directory

--- Auto-Scroll/auto-scroll-min.js
+++
@@ -1,1 +0,0 @@
-javascript:if("1"==enbledscroll){var enbledscroll=0;clearInterval(autoscroll)}else{enbledscroll=1;var autoscroll=setInterval(function(){window.scrollTo(0,document.body.scrollHeight)},1500)}
--- Auto-Scroll/auto-scroll.js
+++
@@ -1,9 +0,0 @@
-if (enbledscroll == '1') {
-    var enbledscroll = 0;
-    clearInterval(autoscroll);
-} else {
-    var enbledscroll = 1;
-    var autoscroll = setInterval(function() {
-        window.scrollTo(0, document.body.scrollHeight)
-    }, 1500);
-}
