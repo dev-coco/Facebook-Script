3c9d778b46087a2b2413551c79b9d64a03ad3bea
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-07-06 12:52:07 -0400

    Delete Group-Members directory

--- Group-Members/old-count-group-members.js
+++
@@ -1,2 +0,0 @@
-var count_members = document.querySelectorAll("div > div.uiProfileBlockContent._61ce > div > div:nth-child(2) > div._60ri");
-alert(count_members.length);
--- Group-Members/old-show-group-members.js
+++
@@ -1,9 +0,0 @@
-if (enabled == '1') {
-    var enabled = 0;
-    clearInterval(show_members);
-} else {
-    var enabled = 1;
-    var show_members = setInterval(function() {
-        var inputs = document.getElementsByClassName('pam uiBoxLightblue uiMorePagerPrimary')[0].click();
-    }, 2000);
-}
