843a427f1543bdd04fb1b7b3db55544cfa89ba81
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-07-11 22:00:29 -0400

    Update Printf-Group-Members.js

--- Printf-Group-Members/Printf-Group-Members.js
+++ Printf-Group-Members/Printf-Group-Members.js
@@ -1,9 +1,22 @@
-var g_new_members = document.getElementsByClassName("rq0escxv l9j0dhe7 du4w35lb j83agx80 cbu4d94t buofh1pr tgvbjcpo muag1w35 enqfppq2");
-var get_data = g_new_members[g_new_members.length-1].getElementsByClassName("oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl oo9gr5id gpro0wi8 lrazzd5p");
+var group_members = document.getElementsByClassName('b20td4e0 muag1w35');
+var new_members = group_members[group_members.length-1];
 new_page=window.open('')
+var get_data = new_members.querySelectorAll('div [data-visualcompletion="ignore-dynamic"][style="padding-left: 8px; padding-right: 8px;"]');
 for (var i = 0; i < get_data.length; i++) {
-var user_url = get_data[i].outerHTML.match(/(?<=href=").*?(?=")/g)[0].replace(/https:\/\/.*?\/|\/groups.*?user\/|\//g,"");
-var user_name = get_data[i].outerHTML.match(/(?<=href=".*?>).*(?=<\/a>)/g)[0].replace(/<.*?div>/g,"");
-var uesr_mark = get_data[i].outerHTML.match(/(?<=href=".*?)>.*(?=\/a>)/g)[0].replace(/>.*?>|<\/div/g,"").replace(/>.*?</g,"");
-new_page.document.write(`<table><tbody><tr><td>`+[i+1]+`</td><td>`+user_name+`</td><td>https://www.facebook.com/`+user_url+`</td><td>`+uesr_mark+`</td></tr></tbody></table>`);
+var user_data = get_data[i].getElementsByClassName('ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi a8c37x1j')[0];
+var user_name = user_data.outerHTML.match(/(?<=aria-label=").*?(?=")/g)[0];
+var user_id = user_data.outerHTML.match(/(?<=href=").*?(?=")/g)[0].replace(/.+user|\//g,"");
+var user_avatar = user_data.outerHTML.match(/(?<=href=").*?(?=")/g)[1].replace(/&amp;/g,"&");
+var is_blank = "";
+if(user_avatar.match("t1.30497-1")) {
+var is_blank = "未设置头像";
+}
+try {
+var user_mark = user_data.outerHTML.match(/simplemarker-mark.*?<\/div>/g)[0].replace(/.+">|<.*?>/g,"");
+} catch {
+var user_mark = "";
+}
+var join_date = user_data.outerHTML.match(/(?<=m9osqain">).*?(?=<\/)/g)[0].replace(/<.*?>/g,"");
+var other_info = user_data.outerHTML.match(/(?<=hzawbc8m" dir="auto">).*?(?=<)/g).slice(-1)[0];
+new_page.document.write('<table><tbody><tr><td>'+[i+1]+'</td><td>'+user_name+'</td><td>'+user_id+'</td><td>=IMAGE("'+user_avatar+'")</td><td>'+join_date+'</td><td>'+other_info+'</td><td>'+is_blank+'</td><td>'+user_mark+'</td></tr></tbody></table>')
 }
